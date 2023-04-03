8407bba1a64f14731fc22340ae26414132316253
42MacAntoine <amontalb@student.42lyon.fr>
2023-04-03 11:42:36 +0200

    gestion des touches

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -36,6 +36,18 @@
 # include <sys/stat.h>
 # include <math.h>
 
+/*---------KEY----------------------*/
+
+# define KEY_W 13
+# define KEY_A 0
+# define KEY_S 1
+# define KEY_D 2
+# define KEY_LEFT 123
+# define KEY_RIGHT 124
+# define KEY_DOWN 125
+# define KEY_UP 126
+
+
 /*----------------------------------*/
 
 # define FALSE 0
