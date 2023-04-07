67753678a2fb3268778d0c8214de97cf6bd2ebaa
Alric42Mac <ale-cont@student.42lyon.fr>
2023-04-07 17:53:11 +0200

    up

--- srcs/raycasting/floor_ceiling.c
+++ srcs/raycasting/floor_ceiling.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   floor_ceiling.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amontalb <amontalb@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ale-cont <ale-cont@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 09:37:00 by amontalb          #+#    #+#             */
-/*   Updated: 2023/04/07 17:35:51 by amontalb         ###   ########.fr       */
+/*   Updated: 2023/04/07 17:52:31 by ale-cont         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,14 @@
 
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
-	char	*dst;
-	dst = &data->mlx->addr[(y * data->mlx->width) * 4  + x * 4];
-	*(unsigned int*)dst = color;
+	char *dst;
+	
+	if (data == NULL || data->mlx == NULL || data->mlx->addr == NULL)
+        return;
+    if (x < 0 || x >= data->mlx->width || y < 0 || y >= data->mlx->height)
+        return;
+    dst = &data->mlx->addr[(y * data->mlx->width) * 4 + x * 4];
+    *(unsigned int *)dst = color;
 }
 
 int floor_ceiling(t_data *d)
