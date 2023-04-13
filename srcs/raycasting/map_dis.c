dfe218b5574f80ee7c5fc79c7addd07e6778dc0a
Alric42Mac <ale-cont@student.42lyon.fr>
2023-04-13 15:59:06 +0200

    up

--- srcs/raycasting/map_dis.c
+++ srcs/raycasting/map_dis.c
@@ -6,11 +6,42 @@
 /*   By: ale-cont <ale-cont@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/12 15:35:39 by ale-cont          #+#    #+#             */
-/*   Updated: 2023/04/13 14:54:10 by ale-cont         ###   ########.fr       */
+/*   Updated: 2023/04/13 15:58:41 by ale-cont         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+static void draw_arrow_player(t_data *d, int x, int y, int color)
+{
+	int dx;
+	int dy;
+	int pixel_x;
+	int pixel_y;
+	int arrow_size;
+
+	dy = -1;
+	arrow_size = 5;
+	while (++dy < 10)
+	{
+		dx = -1;
+		while (++dx < 10)
+		{
+			pixel_x = x * 10 + dx + 22;
+			pixel_y = y * 10 + dy + 22;
+			if (d->player->orientation == 'W' && dx >= arrow_size)
+				my_mlx_pixel_put(d, pixel_x, pixel_y, color);
+			else if (d->player->orientation == 'E' && dx <= 10 - arrow_size)
+				my_mlx_pixel_put(d, pixel_x, pixel_y, color);
+			else if (d->player->orientation == 'S' && dy >= arrow_size)
+				my_mlx_pixel_put(d, pixel_x, pixel_y, color);
+			else if (d->player->orientation == 'N' && dy <= 10 - arrow_size)
+				my_mlx_pixel_put(d, pixel_x, pixel_y, color);
+			else
+				my_mlx_pixel_put(d, pixel_x, pixel_y, 0x00FF00);
+		}
+	}
+}
 
 static	void printmap(t_data *d, int x, int y, int color)
 {
@@ -30,6 +61,8 @@ static	void printmap(t_data *d, int x, int y, int color)
 			my_mlx_pixel_put(d, pixel_x + 22, pixel_y + 22, color);
 		}
 	}
+	if (color == 0xFF0000)
+		draw_arrow_player(d, x, y, color);
 }
 
 static void	dis_map(t_data *d, int zoomed[2], int pos[2], int size)
