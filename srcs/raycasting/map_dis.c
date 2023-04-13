be8b4dfb5225a26aaa83c9d82551e2f10e49c658
Alric42Mac <ale-cont@student.42lyon.fr>
2023-04-13 16:24:12 +0200

    end dismap

--- srcs/raycasting/map_dis.c
+++ srcs/raycasting/map_dis.c
@@ -6,22 +6,20 @@
 /*   By: ale-cont <ale-cont@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/12 15:35:39 by ale-cont          #+#    #+#             */
-/*   Updated: 2023/04/13 16:10:44 by ale-cont         ###   ########.fr       */
+/*   Updated: 2023/04/13 16:22:52 by ale-cont         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-static void draw_arrow_player(t_data *d, int x, int y, int color)
+static void	draw_arrow_player(t_data *d, int x, int y, int color)
 {
-	int dx;
-	int dy;
-	int pixel_x;
-	int pixel_y;
-	int arrow_size;
+	int	dx;
+	int	dy;
+	int	pixel_x;
+	int	pixel_y;
 
 	dy = 1;
-	arrow_size = 5;
 	while (++dy < 8)
 	{
 		dx = 1;
@@ -29,13 +27,13 @@ static void draw_arrow_player(t_data *d, int x, int y, int color)
 		{
 			pixel_x = x * 10 + dx + 22;
 			pixel_y = y * 10 + dy + 22;
-			if (d->player->orientation == 'W' && dx >= arrow_size)
+			if (d->player->orientation == 'W' && dx >= 5)
 				my_mlx_pixel_put(d, pixel_x, pixel_y, color);
-			else if (d->player->orientation == 'E' && dx <= 10 - arrow_size)
+			else if (d->player->orientation == 'E' && dx <= 10 - 5)
 				my_mlx_pixel_put(d, pixel_x, pixel_y, color);
-			else if (d->player->orientation == 'S' && dy >= arrow_size)
+			else if (d->player->orientation == 'N' && dy >= 5)
 				my_mlx_pixel_put(d, pixel_x, pixel_y, color);
-			else if (d->player->orientation == 'N' && dy <= 10 - arrow_size)
+			else if (d->player->orientation == 'S' && dy <= 10 - 5)
 				my_mlx_pixel_put(d, pixel_x, pixel_y, color);
 			else
 				my_mlx_pixel_put(d, pixel_x, pixel_y, 0x00FF00);
@@ -43,12 +41,12 @@ static void draw_arrow_player(t_data *d, int x, int y, int color)
 	}
 }
 
-static	void printmap(t_data *d, int x, int y, int color)
+static void	printmap(t_data *d, int x, int y, int color)
 {
-	int dx;
-	int dy;
-	int pixel_x;
-	int pixel_y;
+	int	dx;
+	int	dy;
+	int	pixel_x;
+	int	pixel_y;
 
 	dy = -1;
 	while (++dy < 10)
@@ -67,10 +65,10 @@ static	void printmap(t_data *d, int x, int y, int color)
 
 static void	dis_map(t_data *d, int zoomed[2], int pos[2], int size)
 {
-	int tile_x;
-	int tile_y;
-	int x;
-	int y;
+	int	tile_x;
+	int	tile_y;
+	int	x;
+	int	y;
 	int	color;
 
 	y = -1;
@@ -86,9 +84,8 @@ static void	dis_map(t_data *d, int zoomed[2], int pos[2], int size)
 				color = 0x000000;
 			else if (tile_x == pos[0] && tile_y == pos[1])
 				color = 0xFF0000;
-			else if (ft_strchr("NSWE ", d->map[tile_y][tile_x]))
-				color = 0x888888;
-			else if (d->map[tile_y][tile_x] == '0')
+			else if (ft_strchr("NSWE ", d->map[tile_y][tile_x])
+				|| d->map[tile_y][tile_x] == '0')
 				color = 0x888888;
 			printmap(d, x, y, color);
 		}
@@ -97,14 +94,16 @@ static void	dis_map(t_data *d, int zoomed[2], int pos[2], int size)
 
 void	display_map(t_data *d)
 {
-	int zoomed[2];
-	int pos[2];
+	int	zoomed[2];
+	int	pos[2];
 	int	size;
 
 	pos[0] = d->player->y;
 	pos[1] = d->player->x;
-	if (d->sizex >  40 && d->sizey > 40)
+	if (d->sizex > 40 && d->sizey > 40)
 		size = 26;
+	else if (d->sizex <= 5 || d->sizey <= 5)
+		return ;
 	else if (d->sizex <= d->sizey)
 		size = d->sizex;
 	else if (d->sizey < d->sizex)
